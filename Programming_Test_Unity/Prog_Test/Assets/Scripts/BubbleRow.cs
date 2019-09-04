86b640a4a8bd34f8ba95620fdc1986594443f0bb
SvenPCD <SvenDemeester@outlook.com>
2019-09-04 15:45:56 +0200

    Unity project made

---
+++ Programming_Test_Unity/Prog_Test/Assets/Scripts/BubbleRow.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleRow : MonoBehaviour
+{
+    public BubbleScript[] BubblesInrow = new BubbleScript[6];
+}
---
+++ Programming_Test_Unity/Prog_Test/Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+{
+    public struct NeighborBubbles
+    {
+        public BubbleScript NE;
+        public BubbleScript NW;
+        public BubbleScript E;
+        public BubbleScript W;
+        public BubbleScript SE;
+        public BubbleScript SW;
+    }
+
+    public NeighborBubbles Neighbors;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
